5bf5a33c0b7b5b75ca4c6c0f79a553bfc32666cf
张子枫 <1048411925@qq.com>
2022-09-06 17:38:58 +0800

    Update user.py

--- user/user.py
+++ user/user.py
@@ -155,7 +155,7 @@ async def funCX(name, scriptPath, msg, group, lable=1):
         result = os.popen(cxjc)
         r = result.readlines()
         if r:
-            a = random.randint(60, 180) #队列检测休眠时间
+            a = random.randint(01, 15) #队列检测休眠时间
             msg = await jdbot.edit_message(msg, f"【队列】{group} 的 `[{name}]` 变量当前已在跑，已加入队列等待。本次等待`{a}`秒后再次尝试。可发送【`监控明细`】查询队列情况。")
             if lable < 21:
                 if lable == 1:
